31e050288ce2c9cd7441a15f74e2c7fdbd00d557
Anjef <danjefff1001@gmail.com>
2026-05-19 20:01:36 +0545

    Name Mismatched Fixed

--- client/src/pages/legal/Legal-Terms.jsx
+++ client/src/pages/legal/Legal-Terms.jsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import './Legal.css';
+import './Legals.css';
 
 const TermsOfService = () => (
   <motion.div className="legal-page" initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
